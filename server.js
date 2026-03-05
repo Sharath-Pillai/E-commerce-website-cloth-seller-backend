2e4a2de497f3025302edfbd2613b94376d377b07
sharath pillai <spillai.developer@gmail.com>
2026-03-05 12:12:37 +0530

    productRouter added and used in server.js file

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import connectDB from "./config/mongodb.js";
 import connectCoudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
 
 //App config
 const app = express();
@@ -17,13 +18,14 @@ app.use(cors());
 
 //API endpoints
 app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
 
 app.get("/", (req, res) => {
-  res.send("Api check");
+  res.send("Api check ok");
 });
-app.get("/about", (req, res) => {
-  res.send("About Page");
-});
+// app.get("/about", (req, res) => {
+//   res.send("About Page");
+// });
 
 app.listen(port, () => {
   console.log(`connected to port: ${port}`);
