4abd41e070187037d63b6ceade9b80da629da354
sharath pillai <spillai.developer@gmail.com>
2026-03-05 12:38:25 +0530

    In addproduct fn images are collected in a array and then these array stored in cloudinary, and mongodb through imageUrlwith the help of cloudinary and multer uplod fn. then secured the imageUrl and stored in productData obj then it added in productModel constructor

--- controllers/productController.js
+++ controllers/productController.js
@@ -90,15 +90,15 @@ const removeProduct = async (req, res) => {
 
 //fn for single product info
 const singleProduct = async (req, res) => {
-    try {
-        const {productId}=req.body
-        // const info=await productModel.findById(req.body.id)
-        const product=await productModel.findById(productId)
-        res.json({success:true,product})
-    } catch (error) {
-        console.log(error)
-        res.json({ success: false, message: error.message })
-    }
+  try {
+    const { productId } = req.body;
+    // const info=await productModel.findById(req.body.id)
+    const product = await productModel.findById(productId);
+    res.json({ success: true, product });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
 };
 
 export { addProduct, listProducts, removeProduct, singleProduct };
