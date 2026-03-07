bd114583374f8e473cf36b811a6eb3f4a03db260
sharath pillai <spillai.developer@gmail.com>
2026-03-07 11:13:31 +0530

    connectDb fn modifed. mongoose.connect and mongoose.connection.on methods modified and used

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,13 +1,23 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+ 
+  mongoose.connection.on("connected", () => {
+    console.log("db connected");
+  });
+  mongoose.connection.on("error", (err) => {
+    console.log("db connection error:", err);
+  });
+  mongoose.connection.on("disconnected", () => {
+    console.log("db disconnected");
+  });
+
   try {
-    await mongoose.connection.on("connected", () => {
-      console.log("db connected");
-    });
-    mongoose.connect(`${process.env.MONGO_CONNECTION_URL}/E-commereceBROTOTYPE`);
+    await mongoose.connect(
+      `${process.env.MONGO_CONNECTION_URL}/E-commereceBROTOTYPE`,
+    );
   } catch (error) {
-    console.log("Database connection failed",error)
+    console.error("Database connection failed", error);
   }
 };
 
