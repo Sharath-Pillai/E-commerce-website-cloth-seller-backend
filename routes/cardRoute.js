3492a829ce0ec1e50a6755b9fa53d6dd2e1305ca
sharath pillai <spillai.developer@gmail.com>
2026-03-09 12:31:46 +0530

    cartRoute file created and path created in cartRouter also addToacart,updatecart,getUsercart fn imported

---
+++ routes/cardRoute.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {
+  addToCart,
+  updateCart,
+  getUserCart,
+} from "../controllers/cardControllers.js";
+import authUser from "../middleware/auth.js";
+
+const cartRouter = express.Router();
+
+cartRouter.post("/add", authUser, addToCart);
+cartRouter.post("/update", authUser, updateCart);
+cartRouter.post("/getUser", authUser, getUserCart);
+
+export default cartRouter;
