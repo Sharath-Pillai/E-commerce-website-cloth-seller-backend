d44036b83d731090f99d38506d75b9d54b5ebafd
sharath pillai <spillai.developer@gmail.com>
2026-03-13 14:41:38 +0530

    renamed cartrouter file

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -3,13 +3,13 @@ import {
   addToCart,
   updateCart,
   getUserCart,
-} from "../controllers/cardControllers.js";
+} from "../controllers/cartControllers.js";
 import authUser from "../middleware/authUser.js";
 
 const cartRouter = express.Router();
 
 cartRouter.post("/add", authUser, addToCart);
 cartRouter.post("/update", authUser, updateCart);
-cartRouter.post("/getUser", authUser, getUserCart);
+cartRouter.post("/get", authUser, getUserCart);
 
 export default cartRouter;
