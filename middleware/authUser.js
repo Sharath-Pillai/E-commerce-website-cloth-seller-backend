a8ca17c908177917add9dffbb9858d791bf31b14
sharath pillai <spillai.developer@gmail.com>
2026-05-25 16:53:50 +0530

    authentication modified,token saved,req.userId saved

--- middleware/authUser.js
+++ middleware/authUser.js
@@ -2,16 +2,28 @@ import jwt from "jsonwebtoken";
 
 const authUser = async (req, res, next) => {
   const { token } = req.headers;
+
   if (!token) {
-    return res.json({ success: false, message: "Not Authorized Login Again" });
+    return res.json({
+      success: false,
+      message: "Not Authorized Login Again",
+    });
   }
+
   try {
     const token_decode = jwt.verify(token, process.env.JWT_SECRET);
-    req.body.userId = token_decode.id;
+
+    req.userId = token_decode.id;
+
     next();
   } catch (error) {
     console.log(error);
-    res.json({ success: false, message: error.message });
+
+    res.json({
+      success: false,
+      message: error.message,
+    });
   }
 };
+
 export default authUser;
