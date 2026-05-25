d9d89ebdd728f1413ee0e9d5674dca9577f17bd2
sharath pillai <spillai.developer@gmail.com>
2026-05-25 17:02:42 +0530

    server.js modified

--- server.js
+++ server.js
@@ -17,7 +17,14 @@ connectCoudinary();
 
 //middlewares
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL
+      ? [process.env.FRONTEND_URL, "http://localhost:5173"]
+      : ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+  })
+);
 
 //API endpoints
 app.use("/api/user", userRouter);
